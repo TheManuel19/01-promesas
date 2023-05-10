c195b54f681010c7e298fda0e19eb8bedde452f8
TheManuel19 <104946647+TheManuel19@users.noreply.github.com>
2023-05-09 19:55:21 -0600

    Add files via upload

---
+++ 02-promesas.js
@@ -0,0 +1,53 @@
+function sumarUnoPromesa(numero) {
+    console.log(numero);
+    var promesa = new Promise(function( resolve,reject){
+        if (numero >= 7) {
+            reject("Número muy grande")
+            return;
+        }
+        setTimeout(function(){
+            resolve(numero + 1);
+        },2000)
+    });
+
+    return promesa;
+}
+
+// sumarUnoPromesa(5).then(function(nuevoValor){
+//     console.log(nuevoValor);
+//     sumarUnoPromesa(nuevoValor).then(function(nuevoValor){
+//         console.log(nuevoValor);
+//         sumarUnoPromesa(nuevoValor).then(function(nuevoValor){
+//             console.log(nuevoValor);
+//         })
+//     })
+// })
+
+// sumarUnoPromesa(5).then((nuevoValor)=>{
+//     console.log(nuevoValor);
+//     return sumarUnoPromesa(nuevoValor);
+// })
+// .then((nuevoValor)=>{
+//     console.log(nuevoValor);
+//     return sumarUnoPromesa(nuevoValor);
+// })
+// .then((nuevoValor)=>{
+//     console.log(nuevoValor);
+// })
+
+// sumarUnoPromesa(5).then((nuevoValor)=>{
+//     return sumarUnoPromesa(nuevoValor);
+// })
+// .then(nuevoValor=>sumarUnoPromesa(nuevoValor))
+// .then(sumarUnoPromesa)
+// .catch((err)=>{
+//     console.log(err);
+// })
+
+sumarUnoPromesa(5)
+.then(sumarUnoPromesa)
+.then(sumarUnoPromesa)
+.then(sumarUnoPromesa)
+.catch((err)=>{
+    console.log(err);
+})
